64a8f33cac59e99f52fbb0c8e34925b6e2973f73
ACER <m.fauzi30052004@gmail.com>
2024-10-29 21:04:51 +0700

    add OkHttpRequest.kt

--- app/src/main/java/muhammadfauzi/polbeng/ac/id/githubrequest/OkHttpRequest.kt
+++ app/src/main/java/muhammadfauzi/polbeng/ac/id/githubrequest/OkHttpRequest.kt
@@ -2,9 +2,10 @@ package muhammadfauzi.polbeng.ac.id.githubrequest
 
 import okhttp3.Call
 import okhttp3.Callback
-import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.FormBody
+
 class OkHttpRequest(client: OkHttpClient) {
     private var client = OkHttpClient()
     init {
